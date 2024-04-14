b6c7e19259b0561a8d7f88d9545f639205f11f6e
Walid <walid.astaoui@student-cs.fr>
2024-04-14 19:37:05 +0200

    Ajout de candidat

--- src/modules/candidates/candidates.controller.js
+++ src/modules/candidates/candidates.controller.js
@@ -6,6 +6,21 @@ const CandidatesController = {
         res.json({ result });
     },
 
+    addCandidate: async (req, res) => {
+        const { name, email, phone, cv } = req.body;
+        if (!name || !email || !phone || !cv) {
+            return res.status(400).send("Il manque des données !");
+        }
+
+        try {
+            await CandidatesService.addCandidate(name, email, phone, cv);
+            return res.status(201).send("Candidat ajouté !");
+        } catch (error) {
+            console.log(err);
+            throw err;
+        }
+    },
+
     upsert: async (req, res) => {
         const created = await CandidatesService.upsert(req.body.name, req.body.email, req.body.phone, req.body.cv);
         return res.status(200).send();
--- src/modules/candidates/candidates.service.js
+++ src/modules/candidates/candidates.service.js
@@ -1,10 +1,46 @@
+const pg = require('pg');
+const UnauthorizedError = require("../../common/errors/unauthorized.error");
+const { getAll } = require('./candidates.controller');
+
+const config = {
+    host: "candidate-finder-db.postgres.database.azure.com",
+    user: "asta-admin",
+    password: "Postgre454545Wa",
+    database: "postgres",
+    port: 5432,
+    ssl: true
+};
+
 const CandidatesService = {
     getAll: async () => {
-        // TO DO
+        const client = new pg.Client(config);
+        await client.connect();
+
+        try {
+            const res = await pool.query('SELECT * FROM candidates');
+            return res.rows;
+        } catch (err) {
+            console.log(err);
+            throw err;
+        } finally {
+            await client.end();
+        }
     },
 
-    upsert: async (name, email, phone, cv) => {
-        // TO DO
+    addCandidate: async (name, email, phone, cv) => {
+        const client = new pg.Client(config);
+        await client.connect();
+
+        try {
+            const query = 'INSERT INTO candidates (name, email, phone, cv) VALUES ($1, $2, $3, $4)';
+            const values = [name, email, phone, cv];
+            await client.query(query, values);
+        } catch (err) {
+            console.log(err);
+            throw err;
+        } finally {
+            await client.end();
+        }
     }
 };
 
